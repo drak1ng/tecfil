aab342c33afdef30ed319d346f95748b05c149d2
drak1ng <drak1ng@iMac-de-Drak1ng.local>
2017-03-01 10:03:45 -0300

    Beta 11

---
+++ js/idiomas.js
@@ -0,0 +1,22 @@
+
+	window.localStorage.setItem('linguage_001','As informações foram atualizadas!');
+	window.localStorage.setItem('linguage_002','E-mail');
+	window.localStorage.setItem('linguage_003','Senha');
+	window.localStorage.setItem('linguage_004','Acessar');
+	window.localStorage.setItem('linguage_005','Esqueci minha senha');
+	window.localStorage.setItem('linguage_006','Criar uma nova conta');
+	window.localStorage.setItem('linguage_007','O campo E-mail deve ser preenchido');
+	window.localStorage.setItem('linguage_008','O campo Senha deve ser preenchido');
+	window.localStorage.setItem('linguage_009','O E-mail ou a Senha utilizados não são válidos');
+	window.localStorage.setItem('linguage_010','');
+	window.localStorage.setItem('linguage_011','');
+	window.localStorage.setItem('linguage_012','');
+	window.localStorage.setItem('linguage_013','');
+	window.localStorage.setItem('linguage_014','');
+	window.localStorage.setItem('linguage_015','');
+	window.localStorage.setItem('linguage_016','');
+	window.localStorage.setItem('linguage_017','');
+	window.localStorage.setItem('linguage_018','');
+	window.localStorage.setItem('linguage_019','');
+	window.localStorage.setItem('linguage_020','');
+	
